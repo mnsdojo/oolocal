0138ea8e5a2eb16f19a943a96f13d3768216186e
notageek-guy <manishkumar97430@gmail.com>
2024-07-16 10:22:30 +0530

    setup shadcn and prisma sqlite

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,17 @@
+
+
+
+'use client';
+
+import React from 'react'
+
+import { usePathname } from 'next/navigation';
+import { Button } from './ui/button';
+import { PlusIcon, MessageSquareIcon } from 'lucide-react';
+function Sidebar() {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
